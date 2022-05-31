d92360773c708de48d27cfe2f7fd59a885986519
Manfredi <manfrediorlando@yahoo.it>
2022-05-31 12:54:27 +0200

    Components7

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,8 @@
 import React from "react";
-
+import { Message } from "./Message";
 export class Hello extends React.Component {
     render(){
-        return <h1>"Hello, World!</h1>
+        return <><h1>"Hello, World!</h1>
+                  <Message/></>
     }
 }
