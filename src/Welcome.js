35cffb6e182834ee5bcedd4fd788fb13341d8fb0
Manfredi <manfrediorlando@yahoo.it>
2022-06-02 16:39:28 +0200

    es1

--- src/Welcome.js
+++ src/Welcome.js
@@ -4,9 +4,7 @@ export class Welcome extends React.Component {
     render() {
         return ( 
             <div>
-                {this.props.age > 18 
-                ? <p>Your age is {this.props.age}</p>
-                : <p>You are very young!</p>}
+                {this.props.age > 18 && <p>Your age is {this.props.age}</p>}
             </div>
         )
     }
