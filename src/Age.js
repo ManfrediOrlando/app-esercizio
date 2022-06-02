ec93a41d0d5e15dbbd0d600954c45b4755dff1d2
Manfredi <manfrediorlando@yahoo.it>
2022-06-02 17:27:50 +0200

    es2

--- src/Age.js
+++ src/Age.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Welcome } from "./Welcome";
 
-export class App extends React.Component {
+export class Age extends React.Component {
     render() {
         return ( 
             <div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom";
-import { App } from "./App";
+import { Age } from "./Age";
 
 
 const root = document.querySelector("#root")
-ReactDOM.render(<App/>, root)
+ReactDOM.render(<Age/>, root)
 
