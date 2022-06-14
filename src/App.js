bfb312b7bf6e8cb80a170a6d60a3f399c708f478
Manfredi <manfrediorlando@yahoo.it>
2022-06-14 20:58:16 +0200

    useState2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { HookCounter } from "./HookCounter";
+import { LoginForm } from "./LoginForm";
 
 
 export class App extends React.Component{
     render(){
         return(
                 <div>
+                    <LoginForm/>
                     <HookCounter initialValue={0}/>
                 </div>
         )
