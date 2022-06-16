22c423fe16e400bfbf7e1e6e07853a3502185dd3
Manfredi <manfrediorlando@yahoo.it>
2022-06-16 22:32:26 +0200

    useContext

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { DisplayLanguage } from "./DisplayLanguage";
+import { LanguageContext } from "./LanguageContext";
 
 
-export class App extends React.Component{
-    render(){
+export function App(){
+    const [language, setLanguage] = useState("en")
+
+    function handleChangeLanguage(event){
+        setLanguage(event.target.value)
+    }
         return(
                 <div>
+                    <select value={language} onChange={handleChangeLanguage}>
+                        <option value="en">English</option>
+                        <option value="it">Italiano</option>
+                    </select>
+                    <LanguageContext.Provider value={language}>
+                        <DisplayLanguage/>
+                    </LanguageContext.Provider>
+                    
                     
                 </div>
         )
-    }
 }
